01dbcc0d93a4d68cb04d35c3df655aca95524cd0
peopleAlreadyKnowWhoIAm <nz54ds@gmail.com>
2022-10-06 22:05:27 +0300

    Updated design of the code

--- src/mgraph.cpp
+++ src/mgraph.cpp
@@ -7,7 +7,9 @@ using std::cout, std::endl;
 
 using std::deque;
 
-mGraph::Target::Target(int to, int weight) : to(to), weight(weight) {}
+Target::Target(int to, int weight) : to(to), weight(weight) {}
+
+From::From(int from, int weight) : from(from), weight(weight) {}
 
 mGraph::mGraph() {}
 
@@ -33,48 +35,56 @@ void mGraph::insert(Edge edge) {
 }
 
 void mGraph::bfs(
-    function<bool(const int from, const int current, const unordered_map<int, int>& visited)> fn,
+    function<bool(const int current, const unordered_map<int, From>& visited)> fn,
     bool only_not_visited /*= true*/) {
   // int - first -> current vertex index, int - second -> from vertex index
-  unordered_map<int, int> visited({{root_, -1}});
+  unordered_map<int, From> visited({{root_, From(-1)}});
 
   // in pair: int - first -> current vertex index, int - second -> from vertex index
-  deque<pair<int, int>> queue;
+  struct Edge_{
+    int current;
+    int from;
+  };
+
+  deque<Edge_> queue;
 
   // find all vertex which there is connection from root to them
   // for each iterator check if the key in iterator is valid
   for (auto it = map_of_pathes_.find(root_); it != map_of_pathes_.end() && (*it).first == root_;
        ++it) {
-    queue.push_back({(*it).second.to, (*it).first});
+    queue.push_back(Edge_{
+      .current = (*it).second.to,
+      .from = (*it).first
+      });
   }
 
   while (!queue.empty()) {
-    pair<int, int>& current = queue.front();
+    Edge_& current = queue.front();
     queue.pop_front();
 
     // call fn if flag not set and break on return callback
-    if (!only_not_visited && fn(current.second, current.first, visited)) {
+    if (!only_not_visited && fn(current.current, visited)) {
       break;
     }
 
     // not add to queue if is visited
-    if (visited.find(current.first) != visited.end()) {
+    if (visited.find(current.current) != visited.end()) {
       continue;
     }
 
-    visited.insert({current.first, current.second});
+    visited.insert({current.current, From(current.from)});
 
-    for (auto it = map_of_pathes_.find(current.first);
-         it != map_of_pathes_.end() && (*it).first == current.first; ++it) {
+    for (auto it = map_of_pathes_.find(current.current);
+         it != map_of_pathes_.end() && (*it).first == current.current; ++it) {
       // if candidate `(*it).second.to` now not equal to current from
       //  in other words if it is not graph <=> graph cycle
-      if ((*it).second.to != current.second) {
-        queue.push_back({(*it).second.to, current.first});
+      if ((*it).second.to != current.from) {
+        queue.push_back({(*it).second.to, current.current});
       }
     }
 
     // call fn if flag set and break on return callback
-    if (only_not_visited && fn(current.second, current.first, visited)) {
+    if (only_not_visited && fn(current.current, visited)) {
       break;
     }
   }
@@ -84,9 +94,8 @@ bool mGraph::present_cycle() {
   bool cycled = false;
 
   // closure with cycled variable
-  auto func = [&cycled](int from, int now, const unordered_map<int, int>& visited) {
+  auto func = [&cycled](int now, const unordered_map<int, From>& visited) {
     // for debug
-    cout << "From: " << from << ", now: " << now << endl;
 
     if (visited.find(now) != visited.end()) {
       cycled = true;
